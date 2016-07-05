535ad1cc4f70e0b4d22eb478445f5b699942ba26
KIM DAEHO <rlaeogh89@gmail.com>
2016-07-05 17:01:01 +0900

    Nodejs sample code :  app.js update

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 var express = require('express');
 var app = express();
+app.locals.pretty = true;
+app.set('view engine','jade');
+app.set('views', './views')
 
 app.use(express.static('public'));
+
+app.get('/topic/:id', function(req,res){
+    var topics = [
+      'Javascript is...',
+      'Nodejs is ...',
+      'Express is...'
+    ];
+    var ouput = `
+      <a href="/topic?id=0">JavaScript</a><br>
+      <a href="/topic?id=1">Nodejs</a><br>
+      <a href="/topic?id=2">Express</a><br><br>
+      ${topics[req.params.id]}
+    `
+    res.send(ouput);
+});
+
+app.get('/topic/:id/:mode', function(req,res){
+    res.send(req.params.id+', '+req.params.mode);
+});
+
+app.get('/template',function(req,res){
+    res.render('temp', {time:Date(), title:'Jade'});
+});
 
 app.get('/',function(req,res){
     res.send('Hello Home Page~');
@@ -9,6 +35,30 @@ app.get('/',function(req,res){
 
 app.get('/login',function(req,res){
     res.send('<h1>login please</h1>');
+});
+
+app.get('/dynamic', function(req, res){
+  var lis = '';
+  for(var i=0; i<5; i++){
+    lis = lis + '<li>coding</li>';
+  }
+  var time = Date();
+  var output = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+        Hello, Dynamic!
+        <ul>
+          ${lis}
+        </ul>
+        ${time}
+    </body>
+  </html>`;
+  res.send(output);
 });
 
 app.get('/route',function(req,res){
